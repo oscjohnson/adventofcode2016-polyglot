8c9249b0dd0d63e7ee9a95071ff643ed2cbdbd62
Oscar Johnson <o.johnsson@live.se>
2017-07-16 22:26:04 +0200

    Final working version

--- day08-cobol/move-column.cob
+++ day08-cobol/move-column.cob
@@ -7,27 +7,27 @@
           01 WS-INDEX PIC 9(2) VALUE 0.
           01 WS-HELP PIC 9(2) VALUE 0.
           01 WS-TEMP.
-             05 WS-C1 OCCURS 3 TIMES.
+             05 WS-C1 OCCURS 6 TIMES.
              10 WS-D1 PIC X(1) VALUE '.'.
           01 COUNTER PIC 9(2) VALUE 1.
           LINKAGE SECTION.
           01 WS-TIMES PIC 9(2) VALUE 1.
           01 WS-COLUMN PIC 9(2) VALUE 1.
           01 WS-TABLE.
-             05 WS-A OCCURS 3 TIMES INDEXED BY I.
-             10 WS-C OCCURS 7 TIMES.
+             05 WS-A OCCURS 6 TIMES INDEXED BY I.
+             10 WS-C OCCURS 50 TIMES.
                 15 WS-D PIC X(1) VALUE '.'.
 
        PROCEDURE DIVISION USING WS-TABLE, WS-COLUMN, WS-TIMES.
           PERFORM VARYING WS-WHAT FROM 1 BY 1 UNTIL WS-WHAT>WS-TIMES
-             PERFORM 3 TIMES
+             PERFORM 6 TIMES
                MOVE WS-C(COUNTER, WS-COLUMN) TO WS-C1(COUNTER)
                ADD 1 TO COUNTER
               END-PERFORM
 
-            PERFORM VARYING WS-INDEX FROM 1 BY 1 UNTIL WS-INDEX>3
-               IF WS-INDEX = 3
-                  MOVE WS-C1(3) TO WS-C(1, WS-COLUMN)
+            PERFORM VARYING WS-INDEX FROM 1 BY 1 UNTIL WS-INDEX>6
+               IF WS-INDEX = 6
+                  MOVE WS-C1(6) TO WS-C(1, WS-COLUMN)
                ELSE
                   SET WS-HELP TO WS-INDEX
                   ADD 1 to WS-HELP
